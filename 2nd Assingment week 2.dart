00a971df90071e7fcdef4b6a060361bed4b501d9
Dera2024 <159682576+Dera2024@users.noreply.github.com>
2024-03-20 09:49:29 +0100

    Add files via upload

---
+++ 2nd Assingment week 2.dart
@@ -0,0 +1,29 @@
+void main() {
+  // Integer data type
+  int age = 30;
+  print("Age: $age");
+
+  // Double data type
+  double height = 5.9;
+  print("Height: $height");
+
+  // String data type
+  String name = "John Doe";
+  print("Name: $name");
+
+  // Boolean data type
+  bool isStudent = true;
+  print("Is Student: $isStudent");
+
+  // List data type (List of integers)
+  List<int> numbers = [1, 2, 3, 4, 5];
+  print("Numbers: $numbers");
+
+  // Map data type (Map of String keys and int values)
+  Map<String, int> scores = {
+    'Math': 90,
+    'Science': 85,
+    'English': 88,
+  };
+  print("Scores: $scores");
+}
